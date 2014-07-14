25a8c14ee81def324202f0f903b82a0e83666c70
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-07-14 19:25:29 +0400

    Init

---
+++ library/MTLPrelude.hs
@@ -0,0 +1,101 @@
+-- |
+-- Reexports of most definitions from \"mtl\" and \"transformers\".
+-- 
+-- For details check out the source.
+module MTLPrelude
+( 
+  module Exports,
+)
+where
+
+
+-- Cont
+-------------------------
+
+import Control.Monad.Cont.Class
+  as Exports
+
+import Control.Monad.Trans.Cont
+  as Exports
+  hiding (callCC)
+
+
+-- Except
+-------------------------
+
+import Control.Monad.Error.Class
+  as Exports
+
+import Control.Monad.Trans.Except
+  as Exports
+  (ExceptT(ExceptT), Except, except, runExcept, runExceptT, 
+   mapExcept, mapExceptT, withExcept, withExceptT)
+
+
+-- Identity
+-------------------------
+
+import Data.Functor.Identity
+  as Exports
+
+
+-- IO
+-------------------------
+
+import Control.Monad.IO.Class
+  as Exports
+
+
+-- Reader
+-------------------------
+
+import Control.Monad.Reader.Class
+  as Exports
+
+import Control.Monad.Trans.Reader
+  as Exports 
+  (Reader, runReader, mapReader, withReader,
+   ReaderT(ReaderT), runReaderT, mapReaderT, withReaderT)
+
+
+-- RWS
+-------------------------
+
+import Control.Monad.RWS.Class
+  as Exports
+
+import Control.Monad.Trans.RWS.Strict 
+  as Exports
+  (RWS, rws, runRWS, evalRWS, execRWS, mapRWS, withRWS,
+   RWST(RWST), runRWST, evalRWST, execRWST, mapRWST, withRWST)
+
+
+-- State
+-------------------------
+
+import Control.Monad.State.Class
+  as Exports
+
+import Control.Monad.Trans.State.Strict
+  as Exports
+  (State, runState, evalState, execState, mapState, withState,
+   StateT(StateT), runStateT, evalStateT, execStateT, mapStateT, withStateT)
+
+
+-- Trans
+-------------------------
+
+import Control.Monad.Trans.Class
+  as Exports
+
+
+-- Writer
+-------------------------
+
+import Control.Monad.Writer.Class
+  as Exports
+
+import Control.Monad.Trans.Writer.Strict
+  as Exports
+  (Writer, runWriter, execWriter, mapWriter, 
+   WriterT(..), execWriterT, mapWriterT)
